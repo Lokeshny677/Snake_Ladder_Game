56960dfee5d33d52898e4f99fa1726ce768ac622
Lokeshny677 <lokeshny677@gmail.com>
2022-07-29 21:04:48 +0530

    Intial commit

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelab.snakeladder;
+import java.util.Random;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake ladder game");
+        int player =0;
+
+    }
+}
