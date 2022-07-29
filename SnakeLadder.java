faacfc54a183732e0e60bfb4bb1b2eac5f51aeed
Lokeshny677 <lokeshny677@gmail.com>
2022-07-29 21:07:00 +0530

    Added:random die roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,6 +5,10 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to snake ladder game");
         int player =0;
+        Random random =new Random();
+        int dieroll = random.nextInt(7);
+        System.out.println("Rolling the die");
+        System.out.println("rolled number " + dieroll);
 
     }
 }
