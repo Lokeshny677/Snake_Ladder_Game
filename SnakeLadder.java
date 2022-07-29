bc638509c493813d6cb878131e94c73e3688e1e6
Lokeshny677 <lokeshny677@gmail.com>
2022-07-29 21:45:03 +0530

    Added:player position No play ,ladder

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,8 +7,22 @@ public class SnakeLadder {
         int player =0;
         Random random =new Random();
         int dieroll = random.nextInt(7);
+        int option =random.nextInt();
         System.out.println("Rolling the die");
-        System.out.println("rolled number " + dieroll);
+        System.out.println("Player position " + dieroll);
 
+        switch (option){
+            case 1:
+                System.out.println("No play");
+                break;
+            case 2:
+                System.out.println("Ladder");
+                player +=dieroll;
+
+            case 3:
+                System.out.println("Snake bite");
+                player -=dieroll;
+                break;
+        }
     }
 }
